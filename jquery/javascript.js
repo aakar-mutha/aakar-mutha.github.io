d9aa639ba87e861fa4f14160f21f2f5f401260c8
marketingharp <aakar.mutha18@gmail.com>
2020-06-15 15:14:47 +0530

    added remaining sections

--- jquery/javascript.js
+++ jquery/javascript.js
@@ -3,9 +3,20 @@ function checkAddress(checkbox)
     if (checkbox.checked)
     {
       $(".showcase").hide();
+      $(".experience").hide();
+      $(".education").hide();
+      $(".skills").hide();
+      $(".interests").hide();
+      $(".awards").hide();
+
     }
     else
     {
       $(".showcase").show();
+      $(".experience").show();
+      $(".education").show();
+      $(".skills").show();
+      $(".interests").show();
+      $(".awards").show();
     }
 }   
